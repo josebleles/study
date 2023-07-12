c2284aa0664398035d6b3eeedc2a5337dc3c8062
Jose Leles <jose.b.leles@gmail.com>
2023-07-12 17:15:33 -0300

    paypal

--- payment/stripe/src/index.ts
+++ payment/stripe/src/index.ts
@@ -6,10 +6,28 @@ import 'dotenv/config';
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 async function teste() {
   try{
-    const payout = await stripe.payouts.create({
-    amount: 1100,
-      currency: 'usd',
+    // const payout = await stripe.payouts.create({
+    // amount: 1100,
+    //   currency: 'usd',
+    // });
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: 'T-shirt',
+            },
+            unit_amount: 2000,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: 'http://localhost:4242/success',
+      cancel_url: 'http://localhost:4242/cancel',
     });
+    console.log(session);
   }catch(err){
     console.log(err);
   }
